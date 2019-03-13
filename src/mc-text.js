5fd07ed8de9eb5f94caa38aef93ee16c49f3abc8
Roel van Hintum <roel@born05.com>
2019-03-13 16:05:41 +0100

    Updated components and made them self registering

--- src/mc-text.js
+++ src/mc-text.js
@@ -1,60 +1,22 @@
-import { BodyComponent } from 'mjml-core'
+import MjText from 'mjml-text'
+import { registerDependencies } from 'mjml-validator'
 
-import conditionalTag from 'mjml-core/lib/helpers/conditionalTag'
+registerDependencies({
+  'mj-column': ['mc-text'],
+  'mj-hero': ['mc-text'],
+  'mc-text': [],
+});
 
-export default class McText extends BodyComponent {
-  static endingTag = true
-
+export default class McText extends MjText {
   static allowedAttributes = {
+    ...MjText.allowedAttributes,
     'mc:edit': 'string',
     'mc:hideable': 'string',
-    'align': 'enum(left,right,center)',
-    'background-color': 'color',
-    'color': 'color',
-    'container-background-color': 'color',
-    'font-family': 'string',
-    'font-size': 'unit(px,%)',
-    'font-style': 'string',
-    'font-weight': 'string',
-    'height': 'unit(px,%)',
-    'letter-spacing': 'unit(px,%)',
-    'line-height': 'unit(px,%)',
-    'padding-bottom': 'unit(px,%)',
-    'padding-left': 'unit(px,%)',
-    'padding-right': 'unit(px,%)',
-    'padding-top': 'unit(px,%)',
-    'padding': 'unit(px,%){1,4}',
-    'text-decoration': 'string',
-    'text-transform': 'string',
-    'vertical-align': 'string',
   }
 
   static defaultAttributes = {
-    'align': 'left',
-    'color': '#000000',
-    'font-family': 'Ubuntu, Helvetica, Arial, sans-serif',
-    'font-size': '13px',
-    'line-height': '1',
-    'padding': '10px 25px',
+    ...MjText.defaultAttributes,
     'mc:hideable': false
-  }
-
-  getStyles() {
-    return {
-      text: {
-        'font-family': this.getAttribute('font-family'),
-        'font-size': this.getAttribute('font-size'),
-        'font-style': this.getAttribute('font-style'),
-        'font-weight': this.getAttribute('font-weight'),
-        'letter-spacing': this.getAttribute('letter-spacing'),
-        'line-height': this.getAttribute('line-height'),
-        'text-align': this.getAttribute('align'),
-        'text-decoration': this.getAttribute('text-decoration'),
-        'text-transform': this.getAttribute('text-transform'),
-        'color': this.getAttribute('color'),
-        'height': this.getAttribute('height'),
-      },
-    }
   }
 
   isHideable() {
@@ -83,29 +45,17 @@ export default class McText extends BodyComponent {
     `
   }
 
-  render() {
-    const height = this.getAttribute('height')
-    let attrs = {
-      'role': 'presentation',
-      'border': 0,
-      'cellpadding': 0,
-      'cellspacing': 0
-    }
-    if (this.isHideable()) {
-      attrs['mc:hideable'] = true
-    }
-
-    return height
-      ? `
-        ${conditionalTag(`
-          <table
-            ${this.htmlAttributes(attrs)}><tr><td height="${height}" style="vertical-align:top;height:${height};">
-        `)}
-        ${this.renderContent(true)}
-        ${conditionalTag(`
-          </td></tr></table>
-        `)}
-      `
-      : this.renderContent()
+  renderContent() {
+    return `
+      <div
+        ${this.htmlAttributes({
+          style: 'text',
+          'mc:edit': this.getAttribute('mc:edit'),
+          'mc:hideable': this.getAttribute('mc:hideable') ? 'mc:hideable' : null,
+        })}
+      >
+        ${this.getContent()}
+      </div>
+    `
   }
 }
